b002300e96e9279ad77fe7571bb641a532b6e647
Dennis Mortensen <stinarn@gmail.com>
2015-07-30 02:33:08 +0200

    Implemented random + duration effects

--- backend/services/BaseCollectionService.php
+++ backend/services/BaseCollectionService.php
@@ -72,6 +72,8 @@ class BaseCollectionService {
 
     public function insert($doc)
     {
+        if(!isset($doc['_id']))
+            $doc['_id'] = null;
         $id = ($this->generatedId) ? new MongoId($doc['_id']) : $doc['_id'];
         $existing = $this->collection->findOne(array('_id' => $id));
         if(!empty($existing))
